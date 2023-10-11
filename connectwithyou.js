8bff8786e376091583374d354c2127edb4076a21
progax01 <anurag35412@gmail.com>
2023-10-11 13:16:21 +0530

    modified changes

--- connectwithyou.js
+++ connectwithyou.js
@@ -2,9 +2,10 @@ const TelegramBot = require("node-telegram-bot-api");
 const token = "6454018119:AAFZQfwfCggrtToY3OTJSBwkUwS7PQDv7uQ"; // Replace with your actual bot token
 const bot = new TelegramBot(token, { polling: true });
 const { Web3 } = require("web3");
-const infuraUrl =
-  "https://sepolia.infura.io/v3/da8932a090e84bc8ac665b643d5bf539"; // Replace with your Infura API key
+const infuraUrl ="https://sepolia.infura.io/v3/da8932a090e84bc8ac665b643d5bf539"; // Replace with your Infura API key
 const web3 = new Web3(new Web3.providers.HttpProvider(infuraUrl));
+
+
 //const fetch = require('node-fetch');
 
 const userData = {};
@@ -92,63 +93,53 @@ bot.onText(/Check Ethereum Balance/, (msg) => {
 bot.onText(/Track ERC20/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, "Start tracking ERC20 Token deployment");
- 
-  // async function checkForNewERC20Tokens() {
-  //   const latestBlock = await web3.eth.getBlock('latest');
-  
-  //   for (const txHash of latestBlock.transactions) {
-  //     const tx = await web3.eth.getTransaction(txHash);
-  //     const receipt = await web3.eth.getTransactionReceipt(txHash);
 
-  //     if (tx.to === null && receipt.contractAddress) {
+  async function checkForNewERC20Tokens() {
+    const latestBlockNumber = await web3.eth.getBlockNumber();
+    console.log("block");
+    const block = await web3.eth.getBlock(latestBlockNumber, true);
 
-  //       bot.sendMessage(chatId, `New ERC20 token deployed!\nContract Address: ${receipt.contractAddress}`);
-  //     }
-  //   }
-  // }
+    if (!block || !block.transactions) {
+      return;
+    }
 
-  
-// Function to check for new ERC20 token deployments
-async function checkForNewERC20Tokens() {
-  const latestBlockNumber = await web3.eth.getBlockNumber();
-  console.log("block")
-  const block = await web3.eth.getBlock(latestBlockNumber, true);
+    // Iterate through the transactions in the latest block
+    for (const tx of block.transactions) {
+      const txReceipt = await web3.eth.getTransactionReceipt(tx.hash);
 
-  if (!block || !block.transactions) {
-    return;
-  }
+      let  isERC20Token = false;
 
-  // Iterate through the transactions in the latest block
-  for (const tx of block.transactions) {
-    const txReceipt = await web3.eth.getTransactionReceipt(tx.hash);
+      // Get the contract code
+    const contractCode = await web3.eth.getCode(txReceipt.contractAddress);
 
-    if (tx.to === null && txReceipt.contractAddress  && await isERC20Token(txReceipt.contractAddress)) {
-      console.log(txReceipt.contractAddress);
+// Decode the contract code into an ABI object
+    const contractABI = JSON.parse(web3.utils.decodeABI(contractCode));
 
-      bot.sendMessage(chatId, `New ERC20 token deployed!\nContract Address: ${txReceipt.contractAddress}`);
+     
+
+      const ERC20TokenFunctions = ["balanceOf", "transfer","allowance","approve","totalSupply",];
+
+      for (const functionName of ERC20TokenFunctions) {
+        if (!contractABI.includes(functionName)) {
+          isERC20Token = false;
+        } else {
+          isERC20Token = true;
+        }
+ }
+        if (tx.to === null && txReceipt.contractAddress && isERC20Token)
+      {
+          console.log(txReceipt.contractAddress);
+
+          bot.sendMessage(chatId,`New ERC20 token deployed!\nContract Address: ${txReceipt.contractAddress}`);
+        }
+     
     }
   }
 
-}
+  setInterval(checkForNewERC20Tokens, 100); // Check every 1 minute
 
-async function isERC20Token(contractAddress) {
-
-  const contractABI = await web3.eth.getCode(contractAddress);
-
-  
-  const ERC20TokenFunctions = ["balanceOf", "transfer", "allowance", "approve", "totalSupply"];
-  for (const functionName of ERC20TokenFunctions) {
-    if (!contractABI.includes(functionName)) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-  setInterval(checkForNewERC20Tokens, 60000); // Check every 1 minute
-
-  console.log('Bot is running and monitoring Sepolia blockchain for new ERC20 tokens...');
+  console.log("Bot is running and monitoring Sepolia blockchain for new ERC20 tokens..."
+  );
 });
 
 // Handle user messages
@@ -156,6 +147,4 @@ bot.on("message", (msg) => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
   // Handle user messages here, if needed 52249708.
-
-
 });
